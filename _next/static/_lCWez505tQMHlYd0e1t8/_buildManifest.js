85884943727659ba8042b9c48dab6830cbf5af0e
sezeresim <sezeresim@gmail.com>
2021-01-11 21:47:28 +0000

    Deploying to gh-pages from  @ d410f11a045a0e1e5234a8951c386a4a22725bdd 🚀

---
+++ _next/static/_lCWez505tQMHlYd0e1t8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-721dbde7e7af93f98ae4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2157553d569a4a4e7bf2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

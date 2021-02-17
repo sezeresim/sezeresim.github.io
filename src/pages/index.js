a82a5869833ca877d7096a5bd9d513ff64769acf
Sezer Esim <sezeresim@gmail.com>
2021-02-17 23:56:33 +0300

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,8 @@
 import Head from 'next/head';
 import { Box, Container, Text, Image, Link, Badge, Wrap, Avatar, Stack, Divider } from '@chakra-ui/react';
 import { CheckCircleIcon } from '@chakra-ui/icons';
-
+import { Icon } from '../components';
 export default function Home() {
-  const getRandomColor = () => {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
   return (
     <>
       <Head>
@@ -19,29 +10,31 @@ export default function Home() {
         <link rel="icon" href={'/favicon.ico'} />
       </Head>
       <Container maxW="4xl">
-        <Stack spacing={3}>
-          <Text fontSize="2xl" isTruncated color="rebeccapurple">
+        <Stack spacing={3} mb="5">
+          <Text fontSize="xl" isTruncated>
             Hi,I am Sezer,
           </Text>
           <Text>I am a software engineer.</Text>
         </Stack>
-        <Divider my="5" />
-
-        <Text fontSize="2xl" isTruncated color="skyblue" mb="5">
+        <Divider />
+        <Text fontSize="2xl" isTruncated mb="5">
           Tech Stack
         </Text>
         {[
-          { name: 'Javascript', icon: <CheckCircleIcon mr="2" color={getRandomColor} /> },
-          { name: 'Node.js', icon: <CheckCircleIcon mr="2" color={getRandomColor} /> },
-          { name: 'AWS', icon: <CheckCircleIcon mr="2" color={getRandomColor} /> },
-          { name: 'React.js', icon: <CheckCircleIcon mr="2" color={getRandomColor} /> },
-          { name: 'Next.js', icon: <CheckCircleIcon mr="2" color={getRandomColor} /> },
-          { name: 'Serverless', icon: <CheckCircleIcon mr="2" color={getRandomColor} /> },
-          { name: 'React Native', icon: <CheckCircleIcon mr="2" color={getRandomColor} /> },
-          { name: 'NoSQL', icon: <CheckCircleIcon mr="2" color={getRandomColor} /> },
+          { name: 'Javascript', icon: <Icon.Javascript mr="2" /> },
+          { name: 'Node.js', icon: <Icon.Nodejs mr="2" /> },
+          { name: 'AWS', icon: <Icon.Aws mr="2" /> },
+          { name: 'React.js', icon: <Icon.React mr="2" /> },
+          { name: 'Next.js', icon: <Icon.NextjsIcon mr="2" /> },
+          { name: 'Serverless', icon: <Icon.Serverless mr="2" /> },
+          { name: 'React Native', icon: <Icon.React mr="2" /> },
+          { name: 'Sass', icon: <Icon.Sass mr="2" /> },
+          { name: 'Css', icon: <Icon.Css3 mr="2" /> },
+          { name: 'Html', icon: <Icon.Html5 mr="2" /> },
+          { name: 'Git', icon: <Icon.Git mr="2" /> },
         ].map((item) => (
-          <Text display="inline-flex" alignItems="center" mr="5" mb="5" color={getRandomColor()}>
-            {item.icon} {item.name}
+          <Text display="inline-flex" fontSize="5xl" alignItems="center" mr="5" mb="5">
+            {item.icon}
           </Text>
         ))}
       </Container>
